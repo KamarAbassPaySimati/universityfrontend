d8cd4fa2e4211c2fa107ab3408c106aa897ed9ef
Ranjith N <ranjith.n@7edge.com>
2024-03-06 18:07:28 +0530

    NN-14 #time 20m feature: Danger removed

--- scripts/envs.sh
+++ scripts/envs.sh
@@ -1,9 +1,9 @@
-parameter_names=($(aws ssm describe-parameters --query "Parameters[*].Name" --output text --region $AWS_REGION))
+parameter_names=($(aws ssm describe-parameters --query "Parameters[*].Name" --output text --region $REGION))
 # Loop through each parameter
 for param_name in "${parameter_names[@]}"; do
     echo "$param_name"
     # Get parameter value
-    param_value=$(aws ssm get-parameter --name "$param_name" --query "Parameter.Value" --output text --region $AWS_REGION)
+    param_value=$(aws ssm get-parameter --name "$param_name" --query "Parameter.Value" --output text --region $REGION)
 
     # Append to .env file
     echo "${param_name##*/}=$param_value" >> .env  # Write key-value pair to .env file
