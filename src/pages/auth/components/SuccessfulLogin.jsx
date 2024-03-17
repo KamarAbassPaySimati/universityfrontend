9a2fa4fd849f3264d7dfd8679a9983d967798ba9
Bharath <bharath.shet@7edge.com>
2024-03-17 14:58:10 +0530

    NN-98 #time 10m bugfix: login updated unit test cases

--- src/pages/auth/components/SuccessfulLogin.jsx
+++ src/pages/auth/components/SuccessfulLogin.jsx
@@ -15,16 +15,16 @@ const SuccessfulLogin = () => {
             <div className='border border-neutral-outline max-w-[627.5px] gap-[27px] rounded-[8px] mb-10 flex p-12'>
                 <Image src='security-icon' />
                 <div className='flex flex-col items-start justify-center font-[400]'>
-                    <h1 className='text-[20px] leading-[28px] text-[#000000]'>
+                    <h1 data-testid="2FA-enabled-header" className='text-[20px] leading-[28px] text-[#000000]'>
                         Successfully Enabled
                     </h1>
-                    <p className='text-[14px] leading-[24px] text-neutral-secondary'>
+                    <p data-testid="2FA-enabled-content" className='text-[14px] leading-[24px] text-neutral-secondary'>
                         Each time you log in using your email and password,
                         please enter the 6-digit code generated from Google Authenticator app
                     </p>
                 </div>
             </div>
-            <Button onClick={handleDone} text='Done' className='w-[167px]' />
+            <Button testId="done_button" onClick={handleDone} text='Done' className='w-[167px]' />
         </>
     );
 };
