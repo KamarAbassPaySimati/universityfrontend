69d3d6076605e93fb426b4e5120697e33633b695
Akshatha <akshatha@7edge.com>
2024-03-19 14:41:31 +0530

    NN-4 #time 10m feature: Update password Update password UI Component Added

--- src/pages/UpdatePassword/UpdatePassword.jsx
+++ src/pages/UpdatePassword/UpdatePassword.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import CardHeader from '../../components/CardHeader';
-import { useNavigate } from 'react-router-dom';
+import UpdateToNewPassword from './Components/UpdateToNewPassword';
 
 const UpdatePassword = () => {
-    const navigate = useNavigate();
     return (
         <CardHeader activePath='Update Password' paths={['Profile']} pathurls={['profile']} >
-            <div className='mx-10 my-8 px-[30px] pt-[24px] pb-[28px] flex flex-col bg-[#FFFFFF]' data-testid="profile_info_card">
-
+            <div className='mx-10 my-8 px-[30px] pt-[24px] pb-[28px] flex flex-col bg-[#FFFFFF]'
+                data-testid="update_password_card">
+                <p>Update Password</p>
                 <div className='flex justify-between border-b border-neutral-outline pb-[18px]'>
-                    <p>Update Password</p>
-                    <div className='flex gap-[27px] justify-center items-center'>
-                    </div>
+                    <UpdateToNewPassword />
+
                 </div>
             </div>
         </CardHeader>
