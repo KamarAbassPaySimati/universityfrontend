450d3354fc215d4b56262102d42b541f63dbbec1
Akshatha <akshatha@7edge.com>
2024-03-29 09:31:24 +0530

    NN-141 #time 10m feature: admin list Admin Table UI Changes

--- src/pages/Users/Admin/Onboard admin/components/AdminTable.jsx
+++ src/pages/Users/Admin/Onboard admin/components/AdminTable.jsx
@@ -18,7 +18,7 @@ const AdminTable = (
         <table className='w-full'>
             <thead className='text-neutral-secondary whitespace-nowrap text-[14px] leading-[24px] '>
                 <tr className=' border-y border-neutral-outline'>
-                    <th className='py-2 px-[10px] text-center font-[400]'>Paymaart ID</th>
+                    <th className='py-2 px-[10px] text-left font-[400]'>Paymaart ID</th>
                     <th className='py-2 px-[10px] text-left font-[400]'>
                         <div className='cursor-pointer flex gap-1 w-fit' onClick={handleSortByName}>
                             <span>Name</span>
@@ -38,12 +38,18 @@ const AdminTable = (
                 : <tbody className='text-neutral-primary whitespace-nowrap text-[14px] leading-[24px] font-[400]'>
                     {List?.data?.map((user, index) => (
                         <tr key={index} className='border-y border-neutral-outline h-[48px]'>
-                            <td className='py-2 px-[10px] text-left'>{user?.paymaart_id || '-'}</td>
-                            <td className='py-2 px-[10px]'>{`${user?.first_name} ${user?.middle_name} ${user?.last_name}`}</td>
-                            <td className='py-2 px-[10px]'>{user?.email}</td>
-                            <td className='py-2 px-[10px]'>{`${user?.country_code} ${formatInputPhone(user?.phone_number)}`}</td>
-                            <td className='py-2 px-[10px] text-left'>{user?.user_type || '-'}</td>
-                            <td className='py-2 px-[10px]'>
+                            <td title = {user?.paymaart_id}
+                                className='py-2 px-[10px] text-left truncate max-w-[50px]'>{user?.paymaart_id || '-'}</td>
+                            <td title = {`${user?.first_name} ${user?.middle_name} ${user?.last_name}`}
+                                className='py-2 px-[10px] text-left truncate max-w-[300px]'>
+                                {`${user?.first_name} ${user?.middle_name} ${user?.last_name}`}</td>
+                            <td title = {user?.email} className='py-2 px-[10px] text-left truncate max-w-[300px]'>{user?.email}</td>
+                            <td title = {`${user?.country_code} ${formatInputPhone(user?.phone_number)}`}
+                                className='py-2 px-[10px] text-left truncate max-w-[300px]'>
+                                {`${user?.country_code} ${formatInputPhone(user?.phone_number)}`}</td>
+                            <td title = {user?.user_type}
+                                className='py-2 px-[10px] text-left truncate'>{user?.user_type || '-'}</td>
+                            <td className='py-2 px-[10px] text-left truncate'>
                                 <span className={ user?.created_at ? isTimestampFiveMinutesAgo(user?.created_at) ? 'text-[#4F5962]' : 'text-accent-positive' : 'text-[#4F5962]'}>
                                     { user?.created_at
                                         ? isTimestampFiveMinutesAgo(user?.created_at)
@@ -54,7 +60,7 @@ const AdminTable = (
                                     }
                                 </span>
                             </td>
-                            <td className='py-2 px-[10px]'>
+                            <td className='py-2 px-[10px] text-left truncate'>
                                 <span className={`py-[2px] px-[10px] text-[13px] font-[600] capitalize 
                                  ${user?.status === 'active'
                             ? 'bg-[#ECFDF5] text-accent-positive'
