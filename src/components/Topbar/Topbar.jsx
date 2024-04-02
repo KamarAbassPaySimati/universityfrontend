021ebd680ea4e5456e7ddf2f419206e2748cee88
Akshatha <akshatha@7edge.com>
2024-04-02 21:37:07 +0530

    NN-141 #time 5m feature: admin list filter issue resolved

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -64,7 +64,9 @@ const Topbar = ({
     const handleClearFilter = () => {
         // Reset filterValues to an empty object or default values
         const params = Object.fromEntries(searchParams);
-        params.page = 1;
+        if (filterActive) {
+            params.page = 1;
+        }
         delete params.role;
         delete params.status;
         setSearchParams({ ...params });
