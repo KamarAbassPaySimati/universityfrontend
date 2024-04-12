db1048ad564bd7d6642c67545faf9f1fb11f98fb
Akshatha <akshatha@7edge.com>
2024-04-12 16:22:17 +0530

    NN-50 #time 30m feature: admin bank accounts Added bank accounts

---
+++ src/components/DocumentTab/DocumentSidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from '../Image/Image';
+
+export default function DocumentSidebar ({ documentTypes }) {
+    const getColor = (type) => {
+        switch (type) {
+        case 'pending':
+            return <Image src='documentError' className="w-7 h-7 green-tick" />;
+        case 'filled':
+            return <div
+                class={'rounded-full  w-7 h-7 flex items-center justify-center bg-accent-positive-secondary'}>
+                <Image src='greenTick' className="w-5 h-5 green-tick" />
+            </div>;
+        case 'clear':
+        default:
+            break;
+        }
+    };
+    return (
+        <div className='w-[30%] border-r-[2px] border-[#000000] h-heightSideBar' >
+            {Object.keys(documentTypes).map((docItem) => (
+                <div className='flex items-center mb-[30px]' key={docItem}>
+                    {/* for the not selected one we have to give text color as neutral-secondary */}
+                    <p className='cursor-pointer font-bold text-[16px] leading-4 text-neutral-primary mr-[18px]'>{docItem}</p>
+                    {getColor(documentTypes[docItem])}
+                </div>))}
+        </div>
+    );
+}
--- src/pages/PaymaartBanks/TrustBanks/index.jsx
+++ src/pages/PaymaartBanks/TrustBanks/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import CardHeader from '../../../components/CardHeader';
+import DocumentSidebar from '../../../components/DocumentTab/DocumentSidebar';
 
 const TrustBanks = () => {
+    const bankTypes = {
+        'Trust Banks': 'clear',
+        'Main Capital': 'clear',
+        Suspense: 'clear',
+        'Transaction fees & Commissions': 'clear',
+        Taxes: 'clear'
+
+    };
     return (
         <CardHeader
             activePath='Trust Banks'
@@ -12,7 +21,20 @@ const TrustBanks = () => {
             headerWithoutButton={false}
             table={false}
         >
-        
+            <div className='flex'>
+                <div className='mt-[52px] ml-[8px] w-full'>
+                <DocumentSidebar
+                    documentTypes={bankTypes}
+                   
+                />
+                </div>
+                <div className='mt-[50px] mb-[100px] w-[1px] ml-[10px] bg-[#000000]'></div>
+              
+                <div>
+                    Empty
+                </div>
+            </div>
+
         </CardHeader>
     );
 };
