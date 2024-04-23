4226dbddba3d51b358d335cf042b8f0221b759d8
KPavitraKini <pavitra.kini@7edge.com>
2024-04-23 19:50:58 +0530

    NN-286 #time 1h bugfix: ui issues and functionality issues are fixed for the full kyc

--- src/components/KYC/KYCRegistrationPopup.jsx
+++ src/components/KYC/KYCRegistrationPopup.jsx
@@ -12,23 +12,42 @@ export default function KYCRegistrationPopup ({ handleClose, isModalOpen }) {
                     <div className='ml-2 text-[#FFF] text-[14px] leading-6 font-normal'>
                         <p className='mt-4'>Paymaart is identifying its customers, via the following Know Your Customer (KYC) process,
                             according to provisions of Malawi’s Financial Crimes (Money Laundering) Regulations, 2019 viz.:</p>
-                        <p className='mt-6'>
-                            1. Every reporting institution shall apply a risk-based approach in identifying its customers in
-                            accordance with section 21 of the Act;</p>
-                        <p className='mt-6'>
-                            2. In applying the requirements of sub-regulation (1), every reporting institution shall ensure that
-                            the results of the risk assessment are consistent with national risk assessment of money
-                            laundering and terrorist financing or other relevant assessments done by supervisory
-                            authorities;
+                        <p className='mt-6 flex'>
+                            <span className='mr-2'>
+                                1. </span>
+                            <span>Every reporting institution shall apply a risk-based approach in identifying its customers in
+                                accordance with section 21 of the Act;</span>
                         </p>
-                        <p className='mt-6'>
-                            3. Where a reporting institution identifies lower risks, the reporting institution may decide to allow
-                            simplified measures for customer due diligence provided that enhanced due diligence shall be
-                            applied where there is suspicion of money laundering, terrorist financing or other financial
-                            crime;                        </p>
-                        <p className='mt-6'>
-                            4. Every reporting institution shall ensure that the results of the institution’s risk assessment are
-                            communicated to all departments or sections of the institution.
+                        <p className='mt-6 flex'>
+                            <span className='mr-2'>
+                                2. </span>
+                            <span>
+                                In applying the requirements of sub-regulation (1), every reporting institution shall ensure that
+                                the results of the risk assessment are consistent with national risk assessment of money
+                                laundering and terrorist financing or other relevant assessments done by supervisory
+                                authorities;
+                            </span>
+
+                        </p>
+                        <p className='mt-6 flex'>
+                            <span className='mr-2'>
+                                3. </span>
+                            <span>
+                                Where a reporting institution identifies lower risks, the reporting institution may decide to allow
+                                simplified measures for customer due diligence provided that enhanced due diligence shall be
+                                applied where there is suspicion of money laundering, terrorist financing or other financial
+                                crime;
+                            </span>
+
+                        </p>
+                        <p className='mt-6 flex'>
+                            <span className='mr-2'>
+                                4.
+                            </span>
+                            <span>
+                                Every reporting institution shall ensure that the results of the institution’s risk assessment are
+                                communicated to all departments or sections of the institution.
+                            </span>
 
                         </p>
                     </div>
