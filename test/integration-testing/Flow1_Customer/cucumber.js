5d249a491129f4b3b9042f1420874b493a240960
Bharath D Shet <bharath.shet@7edge.com>
2024-04-27 15:02:29 +0530

    NN-243 #time 5m feature: updated non-malawi full kyc

--- test/integration-testing/Flow1_Customer/cucumber.js
+++ test/integration-testing/Flow1_Customer/cucumber.js
@@ -2,7 +2,10 @@ module.exports = {
     default: {
         import: ['test'],
         paths: [
-            'test/integration-testing/Flow1_Customer/OnboardCustomer.feature'
+            'test/integration-testing/Flow1_Customer/OnboardCustomer.feature',
+            'test/integration-testing/Flow1_Customer/ListCustomer.feature',
+            'test/integration-testing/Flow1_Customer/MalawiFullKYC.feature',
+            'test/integration-testing/Flow1_Customer/MalawiSimplifiedKYC.feature'
         ]
     }
 };
