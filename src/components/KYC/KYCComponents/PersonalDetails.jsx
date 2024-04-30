b9f13c56c76bfd5e2cb6f69170358e63d5621cf2
KPavitraKini <pavitra.kini@7edge.com>
2024-04-30 14:45:41 +0530

    NN-244 #time 10m feature : bank detais issue is fixed

--- src/components/KYC/KYCComponents/PersonalDetails.jsx
+++ src/components/KYC/KYCComponents/PersonalDetails.jsx
@@ -259,7 +259,7 @@ export default function PersonalDetails ({ handleStates, states, submitSelected,
                 states={states}
                 submitSelected={submitSelected}
             />
-            {role === 'agent' && <FelidDivision
+            {role !== 'merchant' && <FelidDivision
                 divisionObject = {bankInputFelid}
                 handleOnChange={handleStates}
                 states={states}
