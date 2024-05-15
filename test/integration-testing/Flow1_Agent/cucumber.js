97ffd1568a0328ff1b7b1daed16a98a68445e3bb
Bharath D Shet <bharath.shet@7edge.com>
2024-05-15 11:26:56 +0530

    NN-313 #time 15m feature: updated bdd ID updated the bdd parallels updated integration testing

--- test/integration-testing/Flow1_Agent/cucumber.js
+++ test/integration-testing/Flow1_Agent/cucumber.js
@@ -7,7 +7,10 @@ module.exports = {
             'test/integration-testing/Flow1_Agent/MalawiFullAgentKYC.feature',
             'test/integration-testing/Flow1_Agent/MalawiSimplifiedKYC.feature',
             'test/integration-testing/Flow1_Agent/NonMalawiKYC.feature',
-            'test/integration-testing/Flow1_Agent/ViewAgent.feature'
+            'test/integration-testing/Flow1_Agent/ViewAgent.feature',
+            'test/integration-testing/Flow1_Agent/UpdateMalawiFullKYCAgent.feature',
+            'test/integration-testing/Flow1_Agent/UpdateMalawiSimplifiedAgent.feature',
+            'test/integration-testing/Flow1_Agent/UpdateNonMalawiKYCAgent.feature'
         ]
     }
 };
