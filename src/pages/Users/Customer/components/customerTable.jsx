c1130e7bc14d606125937b41dabac8176b344cb3
KPavitraKini <pavitra.kini@7edge.com>
2024-05-22 19:37:03 +0530

    NN-265 #time 10m feature: update issue is resolved

--- src/pages/Users/Customer/components/customerTable.jsx
+++ src/pages/Users/Customer/components/customerTable.jsx
@@ -70,7 +70,7 @@ const CustomerTable = ({ loading, error, List, notFound, searchParams, setSearch
                                         onClick={() => Navigate(`/users/customers/register-customer/specific-view/${user?.paymaart_id}`
                                         )} />
                                     <Image className='cursor-pointer' toolTipId={`edit-${index}`} src='edit' testId={`edit-${index}`}
-                                        onClick={() => user?.kyc_status === 'not_started' ? Navigate(`/users/customers/register-customer/specific-view/update-customer/${user?.paymaart_id}`) : Navigate(`/users/customers/register-customer/kyc-update/${user?.paymaart_id}`)}
+                                        onClick={() => user?.kyc_status === 'not_started' ? Navigate(`/users/customers/register-customer/kyc-registration/${user?.paymaart_id}`) : Navigate(`/users/customers/register-customer/kyc-update/${user?.paymaart_id}`)}
                                     />
                                     <Image className='cursor-pointer' toolTipId={`payin-${index}`} src='payin' />
                                     <Tooltip
