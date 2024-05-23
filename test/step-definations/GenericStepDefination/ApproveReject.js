4f9e35f21a8ecb4238796b543ee5293a138a788a
Bharath D Shet <bharath.shet@7edge.com>
2024-05-23 17:16:34 +0530

    NN-362 #time 30m feature: completed bdd for approve reject delete request

--- test/step-definations/GenericStepDefination/ApproveReject.js
+++ test/step-definations/GenericStepDefination/ApproveReject.js
@@ -1,8 +1,10 @@
+/* eslint-disable camelcase */
 /* eslint-disable max-len */
 const { When, Then } = require('@cucumber/cucumber');
-const { until, By } = require('selenium-webdriver');
+const { Key, until, By } = require('selenium-webdriver');
 const { driver } = require('../1_Driver.js');
 const assert = require('assert');
+const { getModifierKey } = require('../../bdd_modules/index.js');
 
 When('I click on approve {string}', async function (type) {
     // Write code here that turns the phrase above into concrete actions
@@ -11,6 +13,7 @@ When('I click on approve {string}', async function (type) {
     case 'Agent KYC':
     case 'Customer KYC':
     case 'Merchant KYC':
+    case 'Agent Delete Request':
         element = await driver.wait(until.elementLocated(By.css('[data-testid="approve_button"]')));
         await driver.wait(until.elementIsVisible(element));
         await element.click();
@@ -38,6 +41,9 @@ Then('I should see a confirmation prompt for approving {string}', async function
     case 'Merchant KYC':
         assert.equal(modalBody, 'This will allow Merchant to gain access to Paymaart');
         break;
+    case 'Agent Delete Request':
+        assert.equal(modalBody, 'Reason for approval');
+        break;
     default:
         assert.equal(modalBody, 'This will allow Agent to gain access to Paymaart');
         break;
@@ -51,6 +57,7 @@ When('I click on reject {string}', async function (type) {
     case 'Agent KYC':
     case 'Customer KYC':
     case 'Merchant KYC':
+    case 'Agent Delete Request':
         element = await driver.wait(until.elementLocated(By.css('[data-testid="reject_button"]')));
         await driver.wait(until.elementIsVisible(element));
         await element.click();
@@ -77,6 +84,9 @@ Then('I should see a confirmation prompt for reject {string}', async function (t
         modalBody = await driver.wait(until.elementLocated(By.css('[data-testid="modal-body"]'))).getText();
         assert.equal(modalBody, 'Select the reason for rejection');
         break;
+    case 'Agent Delete Request':
+        assert.equal(modalBody, 'Reason for rejection');
+        break;
     default:
         element = await driver.wait(until.elementLocated(By.css('[data-testid="modal"]')));
         await driver.wait(until.elementIsVisible(element));
@@ -85,4 +95,26 @@ Then('I should see a confirmation prompt for reject {string}', async function (t
         assert.equal(modalBody, 'Select the reason for rejection');
         break;
     }
+});
+
+When('I enter the reason for rejecting as {string}', async function (reason) {
+    const element = await driver.wait(until.elementLocated(By.css('[data-testid="reason"]')));
+    await driver.wait(until.elementIsVisible(element));
+
+    await element.sendKeys(Key.chord(getModifierKey(), 'a'), Key.DELETE);
+    await element.sendKeys(reason);
+});
+
+When('I enter the reason for approving as {string}', async function (reason) {
+    const element = await driver.wait(until.elementLocated(By.css('[data-testid="reason"]')));
+    await driver.wait(until.elementIsVisible(element));
+
+    await element.sendKeys(Key.chord(getModifierKey(), 'a'), Key.DELETE);
+    await element.sendKeys(reason);
+});
+
+Then('I should see the delete request status changed to {string}', async function (status) {
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    const actual_status = await driver.wait(until.elementLocated(By.css('[data-testid="status"]'))).getText();
+    assert.equal(actual_status, status);
 });
