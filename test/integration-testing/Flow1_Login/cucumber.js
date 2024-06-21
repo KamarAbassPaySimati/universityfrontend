c7d729e181e82d14b0c0ca7f7fc109f51a898270
rahulhd <rahul.hd@7edge.com>
2024-06-21 12:08:28 +0530

    NN-754 #time 5m feature: testing docs

--- test/integration-testing/Flow1_Login/cucumber.js
+++ test/integration-testing/Flow1_Login/cucumber.js
@@ -3,10 +3,10 @@ module.exports = {
         import: ['test'],
         paths: [
             'test/integration-testing/Flow1_Login/welcome.feature',
-            'test/integration-testing/Flow1_Login/LoginAndViewProfile.feature'
-            // 'test/integration-testing/Flow1_Login/ForgotPassword.feature',
-            // 'test/integration-testing/Flow1_Login/Logout.feature',
-            // 'test/integration-testing/Flow1_Login/UpdatePassword.feature'
+            'test/integration-testing/Flow1_Login/LoginAndViewProfile.feature',
+            'test/integration-testing/Flow1_Login/ForgotPassword.feature',
+            'test/integration-testing/Flow1_Login/Logout.feature',
+            'test/integration-testing/Flow1_Login/UpdatePassword.feature'
         ]
     }
 };
