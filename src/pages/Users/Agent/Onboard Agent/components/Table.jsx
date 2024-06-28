4fb28c0b086d1c52f969778cba01a50c92e81202
Nayana <nayana.sk@7edge.com>
2024-06-28 15:20:32 +0530

    NN-270 #time 30m feature:completed the last logged in task in agent list

--- src/pages/Users/Agent/Onboard Agent/components/Table.jsx
+++ src/pages/Users/Agent/Onboard Agent/components/Table.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Image from '../../../../../components/Image/Image';
 import { formatInputPhone } from '../../../../../CommonMethods/phoneNumberFormat';
 import formatTimestamp from '../../../../../CommonMethods/formatTimestamp';
+import isTimestampFiveMinutesAgo from '../../../../../CommonMethods/lastLoggedInTimeStamp';
 import Shimmer from '../../../../../components/Shimmers/Shimmer';
 import NoDataError from '../../../../../components/NoDataError/NoDataError';
 import { Tooltip } from 'react-tooltip';
@@ -16,24 +17,25 @@ const Table = ({ loading, error, List, notFound, searchParams, setSearchParams, 
         <>
             <table className='w-full min-w-max'>
                 {(List?.data?.length > 0 || loading) &&
-                <thead className='text-neutral-secondary whitespace-nowrap text-[14px] leading-[24px]'>
-                    <tr className='border-b border-neutral-outline sticky top-0 bg-white z-10'>
-                        <th className='py-2 px-[10px] text-left font-[400]'>Paymaart ID</th>
-                        <th className='py-2 px-[10px] text-left font-[400]'>
-                            <div data-testid="sort_agent_name" className='cursor-pointer flex gap-1 w-fit' onClick={() => handleSort('name', searchParams, setSearchParams)}>
-                                <span>Name</span>
-                                <Image src='sort_icon' />
-                            </div>
-                        </th>
-                        <th className='py-2 px-[10px] text-left font-[400]'>Phone Number</th>
-                        <th className='py-2 px-[10px] text-left font-[400]'>Created Date</th>
-                        <th className='py-2 px-[10px] text-left font-[400]'>Status</th>
-                        <th className='py-2 px-[10px]'></th>
-                    </tr>
-                </thead>
+                    <thead className='text-neutral-secondary whitespace-nowrap text-[14px] leading-[24px]'>
+                        <tr className='border-b border-neutral-outline sticky top-0 bg-white z-10'>
+                            <th className='py-2 px-[10px] text-left font-[400]'>Paymaart ID</th>
+                            <th className='py-2 px-[10px] text-left font-[400]'>
+                                <div data-testid="sort_agent_name" className='cursor-pointer flex gap-1 w-fit' onClick={() => handleSort('name', searchParams, setSearchParams)}>
+                                    <span>Name</span>
+                                    <Image src='sort_icon' />
+                                </div>
+                            </th>
+                            <th className='py-2 px-[10px] text-left font-[400]'>Phone Number</th>
+                            <th className='py-2 px-[10px] text-left font-[400]'>Created Date</th>
+                            <th className='py-2 px-[10px] text-left font-[400]'>Last Logged In</th>
+                            <th className='py-2 px-[10px] text-left font-[400]'>Status</th>
+                            <th className='py-2 px-[10px]'></th>
+                        </tr>
+                    </thead>
                 }
                 {loading
-                    ? <Shimmer column={6} row={10} firstIndex/>
+                    ? <Shimmer column={6} row={10} firstIndex />
                     : <tbody className='text-neutral-primary whitespace-nowrap text-[14px] leading-[24px] font-[400]'>
                         {List?.data?.map((user, index) => (
                             <tr key={index} className='border-b border-neutral-outline h-[48px]'>
@@ -41,6 +43,11 @@ const Table = ({ loading, error, List, notFound, searchParams, setSearchParams, 
                                 <td data-testid="agent_name" title={user?.name} className='py-2 px-[10px] truncate min-w-[200px] max-w-[200px]'>{`${user?.name}`}</td>
                                 <td data-testid="phone_number" className='py-2 px-[10px]'>{`${user?.country_code} ${formatInputPhone(user?.phone_number)}`}</td>
                                 <td className='py-2 px-[10px]'>{formatTimestamp(user?.created_at)}</td>
+                                <td className='py-2 px-[10px]'>{user?.last_logged_in
+                                    ? isTimestampFiveMinutesAgo(user?.last_logged_in)
+                                        ? formatTimestamp(user?.last_logged_in)
+                                        : 'Online'
+                                    : '-'}</td>
                                 <td data-testid="status" className='py-2 px-[10px]'>
                                     {user?.status
                                         ? (
@@ -60,7 +67,7 @@ const Table = ({ loading, error, List, notFound, searchParams, setSearchParams, 
                                 <td className='py-3 px-[10px] mr-1 ml-1 flex gap-[19px] text-center align-center justify-end'>
                                     <Image className='cursor-pointer' toolTipId={`eye-${index}`} src='eye' testId={`view-${index}`}
                                         onClick={() => Navigate(`/users/agents/register-agent/specific-view/${user?.paymaart_id}`
-                                        )}/>
+                                        )} />
                                     <Image className='cursor-pointer' toolTipId={`edit-${index}`} src='edit'
                                         onClick={() => user?.kyc_status === 'not_started' ? Navigate(`/users/agents/register-agent/kyc-registration/${user?.paymaart_id}`) : Navigate(`/users/agents/register-agent/kyc-update/${user?.paymaart_id}`)}
                                     />
@@ -89,10 +96,10 @@ const Table = ({ loading, error, List, notFound, searchParams, setSearchParams, 
                 }
             </table>
             {!notFound && error &&
-            (<NoDataError heading='There are no agents added yet' text='Click “Register Agent ” to add agent' />)}
+                (<NoDataError heading='There are no agents added yet' text='Click “Register Agent ” to add agent' />)}
             {List?.data?.length === 0 && !loading &&
-            (searchParams.get('status') !== null || searchParams.get('search') !== null) &&
-            (<NoDataError className='h-tableHeight' heading='No data found' text='Try adjusting your search or filter to find what you’re looking for' />)}
+                (searchParams.get('status') !== null || searchParams.get('search') !== null) &&
+                (<NoDataError className='h-tableHeight' heading='No data found' text='Try adjusting your search or filter to find what you’re looking for' />)}
         </>
     );
 };
