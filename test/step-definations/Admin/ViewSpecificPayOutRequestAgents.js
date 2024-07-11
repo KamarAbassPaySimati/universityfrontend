9c1d9208a91139444e8510ab7aa7bd5d4f4b4d03
Sreekuttan J <sreekuttan.j@7edge.com>
2024-07-11 13:02:55 +0530

    NN-357 #time 45m feature: completed BDD for Specific view of Pay-out requests from agents

---
+++ test/step-definations/Admin/ViewSpecificPayOutRequestAgents.js
@@ -0,0 +1,33 @@
+const { Given, Then, When } = require('@cucumber/cucumber');
+const assert = require('assert');
+const until = require('selenium-webdriver').until;
+const By = require('selenium-webdriver').By;
+const { driver } = require('../1_Driver.js');
+
+When('I click on view pay-out request', async function () {
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    const element = await driver.wait(until.elementLocated(By.css('[data-testid="view-0"]')));
+    await driver.wait(until.elementIsVisible(element));
+
+    this.recipient_paymaart_id = await driver.wait(until.elementLocated(By.css('[data-testid="recipient_paymaart_id"]'))).getText();
+    this.amount = await driver.wait(until.elementLocated(By.css('[data-testid="amount"]'))).getText();
+
+    await element.click();
+});
+
+Then('I should view Pay-out Request Details', async function () {
+    await new Promise(resolve => setTimeout(resolve, 4000));
+
+    console.log('this', this.recipient_paymaart_id, this.amount);
+    const transaction_element = await driver.wait(until.elementLocated(By.css('[data-testid="transaction_details"]')));
+    await driver.wait(until.elementIsVisible(transaction_element));
+
+    const bank_element = await driver.wait(until.elementLocated(By.css('[data-testid="bank_details"]')));
+    await driver.wait(until.elementIsVisible(bank_element));
+
+    const actual_amount = await driver.wait(until.elementLocated(By.css('[data-testid="transaction_details"] [data-testid="amount"]'))).getText();
+    assert.equal(actual_amount, this.amount);
+
+    const actual_recipient_paymaart_id = await driver.wait(until.elementLocated(By.css('[data-testid="transaction_details"] [data-testid="paymaart_id"]'))).getText();
+    assert.equal(actual_recipient_paymaart_id, this.recipient_paymaart_id);
+});
