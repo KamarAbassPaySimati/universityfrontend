e3ec626cd5c947a9c63306d6343cadd1a04bafad
Sreekuttan J <sreekuttan.j@7edge.com>
2024-07-31 11:31:39 +0530

    NN-369 #time 45m feature: completed BDD for viewing insights of agent registration

---
+++ test/step-definations/Admin/InsightsAgentReistration.js
@@ -0,0 +1,23 @@
+const { When } = require('@cucumber/cucumber');
+const { until, By } = require('selenium-webdriver');
+const { driver } = require('../1_Driver.js');
+
+  Given('I navigate to dashboard page',async function () {
+    await driver.get('http://localhost:3000/dashboard');
+    await new Promise(resolve => setTimeout(resolve, 4000));
+  });
+
+  When('I click on export button for {string}',async function () {
+    const button = await driver.wait(until.elementLocated(By.css('[data-testid="export_button"]')));
+    await driver.wait(until.elementIsVisible(button));
+    await button.click();
+  });
+
+  Then('I should see {string} graph',async function (graphType) {
+    switch(graphType){
+        case "Agent Registrations":
+            const graph = await driver.wait(until.elementLocated(By.css('[data-testid="agent-registartion"]')));
+            await driver.wait(until.elementIsVisible(graph));
+    }
+  });
+
