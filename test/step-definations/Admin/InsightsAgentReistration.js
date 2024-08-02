bab607794bb74e3aa44ebcb3d706fdfffacfdddf
Sreekuttan J <sreekuttan.j@7edge.com>
2024-08-02 15:54:18 +0530

    NN-370 #time 25m feature: completed BDD for insights of customer registration

---
+++ test/step-definations/Admin/InsightsAgentReistration.js
@@ -0,0 +1,27 @@
+const { When,Given,Then } = require('@cucumber/cucumber');
+const { until, By } = require('selenium-webdriver');
+const { driver } = require('../1_Driver.js');
+
+  Given('I navigate to dashboard page',async function () {
+    await driver.get('http://localhost:3000/dashboard');
+    await new Promise(resolve => setTimeout(resolve, 4000));
+  });
+
+  When('I click on export button for {string}',async function () {
+    const button = await driver.wait(until.elementLocated(By.css('[data-testid="export_button"]')));
+    await driver.wait(until.elementIsVisible(button));
+    await button.click();
+  });
+
+  Then('I should see {string} graph',async function (graphType) {
+    switch(graphType){
+        case "Agent Registrations":
+            const agentGraph = await driver.wait(until.elementLocated(By.css('[data-testid="agent-registartion"]')));
+            await driver.wait(until.elementIsVisible(agentGraph));
+
+        case "Customer Registrations":
+            const customerGraph = await driver.wait(until.elementLocated(By.css('[data-testid="customer-registartion"]')));
+            await driver.wait(until.elementIsVisible(customerGraph));
+    }
+  });
+
---
+++ test/unit-testing/unit-testing-flow1/Admin/InsightsCustomerReistration.feature
@@ -0,0 +1,20 @@
+Feature: Paymaart- Admin Web- Insights of Customer Registartion
+  As an Super/finance, I want to viewthe insights on Customer Registrations on the application.
+  Condition of satisfaction
+  There should be an option to view the graph with filters such as time period and membership type
+  There should be an option to hover over the data points to see specific values.
+  There should be an option to export the data in.csv
+
+  @add_admin_user
+  @create_new_user_and_login
+  @register_new_agent
+  Scenario: Viewing Insights of Agent Registration
+    Given I navigate to dashboard page
+    Then I should see "Customer Registration" graph
+
+  Scenario: Exporting insights of agent registration
+    Given I navigate to dashboard page
+    When I click on export button for "Customer Registartions"
+    Then I should read a message stating that "Transaction details exported successfully" 
+
+  
