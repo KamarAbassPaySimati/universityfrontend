ab361316db530745302cb72a83b9ff3910f0810d
Nishan Kumar <nishan.kumar@7edge.com>
2024-08-22 14:59:45 +0530

    NN-921 #time 5m feature:Admin Web - Country code for Customer BDD issue solved.

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -5,6 +5,7 @@ import { countyCode } from '../JsonLists/countryCode';
 
 const Dropdown = ({ selectedCode, onChange, error, isFocused }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
 
     const handleDropdownClick = () => {
         setIsOpen(!isOpen);
@@ -13,7 +14,17 @@ const Dropdown = ({ selectedCode, onChange, error, isFocused }) => {
     const handleOptionClick = (code, codeAlpha) => {
         onChange(code, codeAlpha);
         setIsOpen(false);
+        setSearchQuery('');
     };
+
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
+
+    const filteredCountryCodes = countyCode.filter((country) =>
+        country.dial_code.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        country.code.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     return (
         <div className='relative'>
@@ -26,12 +37,21 @@ const Dropdown = ({ selectedCode, onChange, error, isFocused }) => {
                 <img src='/images/chevron-down.svg' className={`ml-1 transition-transform ${isOpen ? 'rotate-180' : ''}`} />
             </div>
             {isOpen && (
-                <div className='absolute z-10 mt-1 w-fit thin-scrollBar max-h-52 overflow-y-auto  bg-white border border-gray-300 rounded-md shadow-lg'>
-                    {countyCode.map((country) => (
+                <div className='absolute z-10 mt-1 w-fit thin-scrollBar max-h-52 overflow-y-auto bg-white border border-gray-300 rounded-md shadow-lg'>
+                    <input
+                        type='text'
+                        value={searchQuery}
+                        data-testid={'change_code_search'}
+                        onChange={handleSearchChange}
+                        placeholder='Search...'
+                        className='w-full px-2 py-2 text-[14px] border-b border-gray-300 focus:outline-none text-primary-normal'
+                    />
+                    {filteredCountryCodes.map((country) => (
                         <div
                             key={country.code}
                             onClick={() => handleOptionClick(country.dial_code, country.code)}
                             className='px-2 py-2 text-[14px] hover:bg-gray-200 cursor-pointer text-primary-normal'
+                            data-testid={'change_code_option'}
                         >
                             {country.dial_code}
                         </div>
