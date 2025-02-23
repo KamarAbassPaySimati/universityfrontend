fbdb7417221cb513caa63b68b7ed38e06c0b8c5b
murali <murali.r@7edge.com>
2025-02-23 13:44:23 +0530

    fixing browser performance

--- src/pages/POC/CustomMarker.jsx
+++ src/pages/POC/CustomMarker.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const CustomMarker = ({ text }) => {
-    const [showTooltip, setShowTooltip] = useState(false);
+// const CustomMarker = ({ text }) => {
+//     const [showTooltip, setShowTooltip] = useState(false);
 
-    const handleMouseEnter = () => {
-        setShowTooltip(true);
-    };
+//     const handleMouseEnter = () => {
+//         setShowTooltip(true);
+//     };
 
-    const handleMouseLeave = () => {
-        setShowTooltip(false);
-    };
+//     const handleMouseLeave = () => {
+//         setShowTooltip(false);
+//     };
 
-    return (
-        <div className="relative">
-            <div
-                className="h-3 w-3 rounded-full bg-red-600 cursor-pointer"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-            ></div>
-            {showTooltip && (
-                <div className="absolute top-0 left-0 mt-4 ml-4 bg-white shadow-md p-2 rounded-md z-10">
-                    <p className="text-red-700">{text}</p>
-                </div>
-            )}
-        </div>
-    );
-};
+//     return (
+//         <div className="relative">
+//             <div
+//                 className="h-3 w-3 rounded-full bg-red-600 cursor-pointer"
+//                 onMouseEnter={handleMouseEnter}
+//                 onMouseLeave={handleMouseLeave}
+//             ></div>
+//             {showTooltip && (
+//                 <div className="absolute top-0 left-0 mt-4 ml-4 bg-white shadow-md p-2 rounded-md z-10">
+//                     <p className="text-red-700">{text}</p>
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
 
-export default CustomMarker;
+// export default CustomMarker;
