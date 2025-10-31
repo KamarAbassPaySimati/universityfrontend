783bffb2aeadff040d5bab155782b776aee0265b
KamarAbassPaySimati <Kamar.abass@afrimax.net>
2025-10-31 16:14:03 +0200

    Remove LottieLoader dependency, use Ant Design Spin

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import image from '../../components/loader.json';
-import Image from '../Image/Image';
-import LoadingDot from '../LottieLoader/LoadingDot';
+import { Spin } from 'antd';
 
 const Loading = () => {
     return (
-        <div className='flex flex-col justify-center items-center h-screen '>
-            <div className='relative'>
-                <Image
-                    src= 'loaderTopImage'
-                    className={'w-[124px] h-[124px] mb-[1px]'}
-                />
-                <LoadingDot
-                    animation={image}
-                    width="152px"
-                    className='absolute top-6 -left-7'
-                />
-            </div>
+        <div className='flex flex-col justify-center items-center h-screen'>
+            <Spin size="large" />
+            <p className="mt-4 text-gray-600">Loading...</p>
         </div>
-
     );
 };
 
